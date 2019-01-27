e59fb55871a8e125979f62e8681ecd479c7b37c8
fourous <fourousky@gmail.com>
2019-01-27 22:43:42 +0800

    webpack

---
+++ webpackdemo/src/TodoList.vue
@@ -0,0 +1,67 @@
+<template>
+<div id="app">
+  <!-- 这里注意，由于模板里只能有一个元素，所以这里需要套一个div -->
+  <div class="all">
+    <input class="item" v-model="inputValue"/>
+
+    <button  class="button"  @click="handlesubmit">提交</button>
+  </div>
+
+  <ul>
+    <!-- 拆分组件 -->
+  <todo-item 
+v-for="(item,index) of list"
+:key="index"
+:content="item"
+:index="index"
+@deleteitem="handledelete"
+  >
+</todo-item>
+  </ul>
+    </div>
+</template>
+
+<script>
+import TodoItem from './components/TodoItem'
+export default {
+ components:{
+   'todo-item':TodoItem
+ },
+
+ data:function(){
+   return {
+     inputValue:'',
+     list:[]
+   }
+ },
+
+ methods:{
+   handlesubmit(){
+     this.list.push(this.inputValue),
+     this.inputValue=""
+   },
+   handledelete:function(index){
+     this.list.splice(index,1)
+   }
+ }
+}
+</script>
+
+<style>
+#app{
+  height: 100%;
+  width:100%;
+}
+.item{
+  color:brown
+}
+.all{
+  text-align: center;
+  margin:0px auto;
+  margin-top: 30rpx;
+}
+.button{
+  display: block;
+  margin: 0px auto;
+}
+</style>
---
+++ webpackdemo/src/components/TodoItem.vue
@@ -0,0 +1,24 @@
+<template>
+ <li class="item" @click="handledelete">{{content}}</li>
+</template>
+<script>
+// 声明父组件接收content属性
+export default {
+props:['content','index'],
+methods:{
+  handledelete:function(){
+    // 这里用广播让父组件收到
+    this.$emit('deleteitem',this.index)
+  }
+}
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<!--这里得scope是只有在这个组件中，才生效，组件中最好是加入scope --> 
+
+<style scoped>
+.item{
+  color: gray;
+}
+</style>
